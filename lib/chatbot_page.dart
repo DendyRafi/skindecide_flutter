edeaba9a8b8edceb68f5a97290551c156d6298fa
DendyRafi <dendyrafi8@gmail.com>
2026-06-02 10:32:35 +0700

    update chatbot

--- lib/chatbot_page.dart
+++ lib/chatbot_page.dart
@@ -23,7 +23,7 @@ class _ChatbotPageState extends State<ChatbotPage> {
   bool _isLoading = false;
 
   static const String _apiKey =
-      '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';
+      '';
   static const String _model = 'llama-3.3-70b-versatile';
 
   static const String _systemPrompt = '''
